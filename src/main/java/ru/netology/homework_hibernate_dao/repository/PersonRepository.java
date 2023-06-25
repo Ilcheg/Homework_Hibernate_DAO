abb1d0d8f5b5a411e80971d0d1fdce968fa3a2e0
Ilya Turkovsky <ilcheg@yandex.ru>
2023-06-25 21:15:25 +0300

    fixed

--- src/main/java/ru/netology/homework_hibernate_dao/repository/PersonRepository.java
+++ src/main/java/ru/netology/homework_hibernate_dao/repository/PersonRepository.java
@@ -13,31 +13,4 @@ public interface PersonRepository extends JpaRepository<Person, String> {
     List<Person> findByAgeIsLessThanOrderByAge(int age);
 
     Optional<Person> findByNameAndSurname(String name, String surname);
-
-    @Override
-    <S extends Person> S save(S entity);
-
-    @Override
-    Optional<Person> findById(String s);
-
-    @Override
-    boolean existsById(String s);
-
-    @Override
-    long count();
-
-    @Override
-    void deleteById(String s);
-
-    @Override
-    void delete(Person entity);
-
-    @Override
-    void deleteAllById(Iterable<? extends String> strings);
-
-    @Override
-    void deleteAll(Iterable<? extends Person> entities);
-
-    @Override
-    void deleteAll();
 }
